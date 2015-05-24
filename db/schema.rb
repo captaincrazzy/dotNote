31f828a29df707962d5e0bb9692d0beaadf2b2bd
SAHIL KHANNA <sahilkhanna.cc@gmail.com>
2015-05-23 21:07:55 -0600

    added username

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150516195114) do
+ActiveRecord::Schema.define(version: 20150524020930) do
 
   create_table "notes", force: :cascade do |t|
     t.string   "title"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20150516195114) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
